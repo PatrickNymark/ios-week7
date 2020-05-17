fc912f17f669407ebd4b78ff6e02e6745c41eac4
Patrick Nymark <patricknymark24@gmail.com>
2020-05-17 15:24:46 +0200

    Initial Commit

---
+++ CustomTableApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTableApplication
+//
+//  Created by Patrick Nymark on 17/05/2020.
+//  Copyright © 2020 Patrick Nymark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
